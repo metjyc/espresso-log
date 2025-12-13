60abf31fb1cb34db5aea1a3730afff1fd9cbf405
metjyc <metjyc@naver.com>
2025-12-13 22:44:06 +0900

    feat: 하단 네비게이션 추가 및 전체적인 스타일 수정

--- src/components/AppShell/Appshell.tsx
+++ src/components/AppShell/Appshell.tsx
@@ -1,12 +1,43 @@
+type TabKey = "home" | "brew" | "logs";
+
 type AppShellProps = {
   children: React.ReactNode;
+  activeTab: TabKey;
 };
 
-export default function AppShell({ children }: AppShellProps) {
+// 타입 객체 선언 다음에 붙어있는 빈배열은 배열 선언을 위함.
+const TABS: { key: TabKey; label: string }[] = [
+  { key: "home", label: "홈" },
+  { key: "brew", label: "추출" },
+  { key: "logs", label: "기록" },
+];
+
+export default function AppShell({ children, activeTab }: AppShellProps) {
   return (
-    <div className="min-h-screen bg-slate-100 flex justify-center items-start">
-      <div className="w-full max-w-[420px] bg-white shadow-lg sm:rounded-2xl overflow-hidden min-h-screen">
-        {children}
+    <div className="min-h-screen bg-neutral-900 flex justify-center">
+      <div className="w-full max-w-[420px] bg-amber-100 shadow-lg sm:rounded-2xl overflow-hidden min-h-screen flex flex-col">
+        <main className="overflow-y-auto px-4 pt-6 pb-20 flex-1 min-h-0">
+          {children}
+        </main>
+
+        <nav className="shrink-0 h-16 bg-white border-t border-black/10">
+          <div className="h-full grid grid-cols-3">
+            {/* TABS.map이 가능한 이유는 위에 TABS를 배열로 선언 해놨기 때문이다.*/}
+            {TABS.map((tab) => {
+              const isActive = activeTab === tab.key;
+
+              return (
+                <button
+                  className={`flex flex-col items-center justify-center text-xs
+            ${isActive ? "text-amber-800 font-semibold" : "text-neutral-400"}
+          `}
+                >
+                  {tab.label}
+                </button>
+              );
+            })}
+          </div>
+        </nav>
       </div>
     </div>
   );
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,8 +2,14 @@ import AppShell from "../components/AppShell/Appshell";
 
 export default function Home() {
   return (
-    <AppShell>
-      <h1 className="text-3xl font-bold text-red-500">This is espresso Log.</h1>
+    <AppShell activeTab="home">
+      <header>
+        <h1>Espresso Log에 오신걸 환영합니다.</h1>
+      </header>
+      <button className="p-3 rounded-xl bg-amber-700 active:bg-amber-800 font-semibold text-white">
+        추출하러 가기
+      </button>
+      <div>최근 샷</div>
     </AppShell>
   );
 }

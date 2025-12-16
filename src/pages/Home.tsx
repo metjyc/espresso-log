362f823a6af851d36d08cef38845a6c7af7674e6
metjyc <metjyc@naver.com>
2025-12-16 09:26:05 +0900

    feat: 버튼 링크 및 스타일 변경

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,28 @@
+import { Coffee } from "lucide-react";
+import { Link } from "react-router-dom";
+
 export default function Home() {
   return (
-    <>
+    <div className="space-y-8">
       <header>
         <h1>Espresso Log에 오신걸 환영합니다.</h1>
       </header>
-      <button className="p-3 rounded-xl bg-amber-700 active:bg-amber-800 font-semibold text-white">
-        추출하러 가기
-      </button>
-      <div>최근 샷</div>
-    </>
+      <Link
+        to="/brew"
+        className="
+        justify-center flex
+        p-3 rounded-xl 
+        bg-amber-700 active:bg-amber-800 
+        font-semibold text-white shadow-md 
+        hover:shadow-lg transition-shadow"
+      >
+        <Coffee size={18} className="px-1" />
+        <span>추출하러 가기</span>
+      </Link>
+      <section className="space-y-3">
+        <h2 className="text-lg font-semibold">최근 샷</h2>
+        <div className="text-sm text-neutral-500">아직 기록이 없어요</div>
+      </section>
+    </div>
   );
 }
